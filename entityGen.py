bf8b820a75cae60f05977220a3ff92b78e625756
srichiardi <s.richiardi@gmail.com>
2015-07-27 14:27:18 +0100

    major improvements

--- entityGen.py
+++ entityGen.py
@@ -1,46 +1,43 @@
 import csv
 
-# entity generator
-class Entity:
-    instances = {}
-    def __init__(self, Type=entType, Id=eID, *args):
-        # if entity type and id already in instances, retrieve instance
-        if eID in Entity.instances[entType].keys():
-            self = Entity.instances[entType][eID]
-        
-        # else create one entity from scratch
-        else:
-            self.type = entType
-            self.group = None
-            self.id = eID
-            self.checked = False
-            self.attributes = {}
-            for arg in args:
-                self.attributes[arg] = []
-            # adding entity to the collection
-            if entType not in Entity.instances.keys():
-                Entity.instances[entType] = { self.id : self }
-            else:
-                Entity.instances[entType][self.id] = self
 
-    def joinGroup(self, newGroup=None):
-        # 2 groups competing
-        if self.group and newGroup:
+class Entity:
+    __instances = {}
+    def __init__(self, entType, entName, attrTypes=[]):
+        self.type = entType
+        self.name = entName
+        self.group = None
+        self.attributes = {}
+        for item in attrTypes:
+            self.attributes[item] = []
+        # adding entity to the collection
+        try:
+            Entity.__instances[self.type][self.name] = self
+        except KeyError:
+            Entity.__instances[self.type] = { self.name : self}
+        
+
+    def joinGroup(self, alienEntity=None):
+        # 2 different groups competing
+        if self.group and alienEntity.group and (self.group.name != alienEntity.group.name):
             # negotiate which group to choose and move the members
-            if self.group.size >= newGroup.size:
-                self.group.annexGroup(newGroup)
+            # local group wins
+            if self.group.size >= alienEntity.group.size:
+                self.group.annexGroup(alienEntity.group)
+            # alien group wins
             else:
-                newGroup.annexGroup(self.group)
+                alienEntity.group.annexGroup(self.group)
         # assigning other group if no own group is set
-        elif not self.group and newGroup:
-            self.group = newGroup
-            newGroup.addMember(self)
-        # assigning his own new group
-        elif not self.group and not newGroup:
-            self.group = Group(Type=self.type)
+        elif not self.group and alienEntity.group:
+            self.group = alienEntity.group
+            self.group.addMember(self)
+        # assigning his own new group if both local and alien groups are none
+        elif not self.group and not alienEntity.group:
+            self.group = Group()
             self.group.addMember(self)
         # if the group was already set by another entity, then do nothing
 
+    
     def getDict(self):
         if self.group:
             groupName = self.group.name
@@ -48,63 +45,92 @@ class Entity:
             groupName = ""
         entityDict = {self.type : self.id,
                       "GROUP" : groupName}
+        
         return entityDict
+    
 
-    def linkAttribute(self, attrType, attrValue):
-        # adding both attribute value to self entity
-        # and adding self entity to attribute entity
-        if attrValue not in self.attributes[attrType]:
-            self.attributes[attrType].append(attrValue)
-            attribute = Entity(attrType, attrValue, self.type)
-            # !!! make sure there are no duplicates!!!!
-            attribute.attributes[self.type].append(self.id)
+    def linkTo(self, attribute):
+        # adding attribute value to self entity
+        if attribute.name not in self.attributes[attribute.type]:
+            self.attributes[attribute.type].append(attribute.name)
+            # and adding self entity to attribute entity
+            attribute.attributes[self.type].append(self.name)
+            
+        # transfer group to attribute here?
+        attribute.joinGroup(self)
+            
     
     def nextNodes(self):
         # find entities corresponding to attributes and return a list of them 
         nodesList = []
         for attrType in self.attributes.keys():
             for attrID in self.attributes[attrType]:
-                nextNode = Entity.instances[attrType][attrID]
+                nextNode = Entity.__instances[attrType][attrID]
                 nodeList.append(nextNode)
 
         return nodesList
+    
+    
+    @classmethod
+    def getEntity(cls, type, name, attrTypes):
+        try:
+            instance = cls.__instances[type][name]
+        except KeyError:
+            instance = cls(type, name, attrTypes)
+            
+        return instance
+        
 
 
 class Group:
     groupCount = 0
-    def __init__(self, Type=groupType):
+    def __init__(self):
         groupCount += 1
-        self.type = groupType
         self.members = []
         self.name = "G%d" % groupCount
         self.size = 0
+
 
     def addMember(self, newMember):
         if newMember not in self.members:
             self.members.append(member)
             self.size += 1
 
+
     def annexGroup(self, otherGroup):
-        self.members.extend(otherGroup)
+        # transfer members from one group to another
+        # and update members' group membership
         for member in otherGroup.members:
+            self.addMember(member)
             member.group = self
+        # remove empty group
         del otherGroup
+
+
 
 ## loading entities from CSV file
 def loadEntities(fileName):
     fileToRead = open(fileName, "rb")
-    csvReader = csv.DictReader(fileToRead)
-    mainEntity = "ebay_acct"
-    attributes = ("iban")
-    for line in fileToRead:
-        # create new main entity if not already existing
-        # or retrieve one that already exhists
-        entity = Entity(Type=mainEntity, Id=line[mainEntity], attributes)
+    csvReader = csv.reader(fileToRead, restval='', delimiter=',', dialect='excel',
+                           quotechar='"')
+    # fetch headers
+    headers = csvReader.next()
+    mainEntType = headers[0]
+    attribTypes = headers[1:]
+    
+    for line in csvReader:
+        # retrieve one that already exists
+        # or create new main entity if not already existing
+        mainEnt = Entity.getEntity(mainEntType, line[0], attribTypes)
+        # create new or confirm current group
+        # only main entities create groups, attributes only receive them and transfer them
+        mainEnt.joinGroup()
         
-        for attrType in attributes:
+        for idx, attrType in enumerate(attributes):
+            attribute = Entity.getEntity(attrType, line[idx], [mainEntType])
             # add attributes to the entity
             # this is a two way operation, saving reference of both in each other
-            entity.linkAttribute(attrType, line[attribute])
+            mainEnt.linkTo(attribute)
                 
     fileToRead.close()
 
