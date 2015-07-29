86172b9c4efb808d2468f629623a66f92aadcba9
srichiardi <s.richiardi@gmail.com>
2015-07-29 13:49:40 +0100

    completed import and export from and to a file methods

--- entityGen.py
+++ entityGen.py
@@ -1,12 +1,15 @@
 from weakref import WeakValueDictionary
 import csv
 import sys
-from docutils.transforms.peps import Headers
 
 
 class Entity:
     __mainEntityType = ''
+    __attributeTypes = []
+    __groupType = 'GROUP'
     __instances = {}
+    
+    
     def __init__(self, entType, entName, attrTypes=[]):
         self.type = entType
         self.name = entName
@@ -42,15 +45,34 @@ class Entity:
         # if the group was already set by another entity, then do nothing
 
     
-    def getDict(self):
+    def getPrintableDicts(self):
         if self.group:
             groupName = self.group.name
         else:
             groupName = ""
-        entityDict = {self.type : self.id,
-                      "GROUP" : groupName}
         
-        return entityDict
+        dictList = []
+        nextAttr = True
+        
+        while nextAttr == True:
+            tempDict = {}
+            nextAttr = False
+            tempDict = { self.type : self.name,
+                        Entity.__groupType : groupName }
+            
+            for key in self.attributes.keys():
+                try:
+                    value = self.attributes[key].pop()
+                    nextAttr = True
+                except IndexError:
+                    value = ''
+                finally:
+                    tempDict[key] = value
+                    
+            if nextAttr:
+                dictList.append(tempDict)
+        
+        return dictList
     
 
     def linkTo(self, attribute):
@@ -87,7 +109,7 @@ class Entity:
     
     
     @classmethod
-    def loadEntities(cls, csvFileName):
+    def importFromFile(cls, csvFileName):
         ''' take a CSV file as input, reads it and create main and attribute entities,
         groups and links in between '''
         fileToRead = open(csvFileName, "rb")
@@ -102,15 +124,30 @@ class Entity:
         if Entity.__mainEntityType != '' and Entity.__mainEntityType != mainEntType:
             fileToRead.close()
             sys.exit("Main Entity conflict: please double check the data being imported!")
+        # or if first import, assign main entity type to class variable
+        elif Entity.__mainEntityType == '':
+            Entity.__mainEntityType = mainEntType
+        
+        # updating the class var attribute types with new types during first and later imports
+        for attribType in attribTypes:
+            if attribType not in Entity.__attributeTypes:
+                Entity.__attributeTypes.append(attribType)
         
         for line in csvReader:
             # retrieve one that already exists
             # or create new main entity if not already existing
             mainEnt = Entity.getEntity(mainEntType, line[0], attribTypes)
+            # only main entities create groups, attributes receive them and transfer them
+            # if a GROUP header exists, assign it at import stage
+            if Entity.__groupType in attribTypes:
+                gIdx = attribTypes.index(Entity.__groupType) + 1
+                importedGroup = Group.getGroupByName(line[gIdx])
+                mainEnt.joinGroup(importedGroup)
             # create new or confirm current group
-            # only main entities create groups, attributes only receive them and transfer them
-            mainEnt.joinGroup()
-            
+            # when no GROUP header is in the imported file
+            else:
+                mainEnt.joinGroup()
+ 
             for idx, attrType in enumerate(attributes):
                 colNr = idx + 1
                 attribute = Entity.getEntity(attrType, line[colNr], [mainEntType])
@@ -130,31 +167,51 @@ class Entity:
     
     
     @classmethod
-    def exportToFile(cls):
+    def exportToFile(cls, folderPath):
         ''' print main entities, relative attributes and groups they belong in CSV format. '''
-        pass
+        fileName = folderPath + "/entithon_export_%s.csv" % datetime.now().strftime("%Y%m%d_%H-%M-%S")
+        csvFileToWrite = open(fileName, 'ab')
+        
+        fieldNames = [Entity.__mainEntityType]
+        fieldNames.extend(Entity.__attributeTypes)
+        fieldNames.append(Entity.__groupType)
+        
+        csvWriter = csv.DictWriter(csvFileToWrite, fieldNames, restval='', delimiter=',',
+                                   extrasaction='ignore', dialect='excel', quotechar='"')
+        csvWriter.writeheader()
+        # iterate through main entities
+        for entity in Entity.__instances[Entity.__mainEntityType].values():
+            entityRecords = entity.getPrintableDicts()
+            csvWriter.writerows(entityRecords)
+            
+        csvFileToWrite.close()
 
 
 class Group:
     __groupCount = 0 # for naming purpose only
     __groupInstances = WeakValueDictionary()
-    def __init__(self):
+    
+    
+    def __init__(self, name=None):
         __groupCount += 1
         self.members = []
-        self.name = "G%d" % __groupCount
+        if name:
+            self.name = name
+        else:
+            self.name = "G-%d" % __groupCount
         self.size = 0
         Group.__groupInstances[self.name] = self
 
 
     def addMember(self, newMember):
+        ''' add new group member entities to the group list '''
         if newMember not in self.members:
             self.members.append(member)
             self.size += 1
 
 
     def annexGroup(self, otherGroup):
-        ''' transfer members from one group to another
-        and update members' group membership '''
+        ''' transfer members from one group to another and update members' group membership '''
         for member in otherGroup.members:
             self.addMember(member)
             member.group = self
@@ -171,25 +228,14 @@ class Group:
         return memberList
     
     
-    def getGroupDicts(self, mainEntityType):
-        ''' return a list of dictionaries ready to be passed to CSV DictWriter '''
-        dictList = []
-        for member in self.members:
-            nextAttr = True
-            while nextAttr == True:
-                tempDict = {}
-                nextAttr = False
-                tempDict[mainEntityType] = member.name
-                for key in member.attributes.keys():
-                    try:
-                        value = member.attributes[key].pop()
-                        nextAttr = True
-                    except IndexError:
-                        value = ''
-                    finally:
-                        tempDict[key] = value
-                if nextAttr:
-                    dictList.append(tempDict)
-                    
-        return dictList
-
+    @classmethod
+    def getGroupByName(cls, groupName):
+        ''' at import stage allows to relink an existing group or create a new one '''
+        # existing group
+        try:
+            group = cls.__groupInstances[groupName]
+        # new group with old imported name
+        except KeyError:
+            group = cls(groupName)
+        return group
+    
